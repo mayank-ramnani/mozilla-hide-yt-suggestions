04b9edd93cfad4c4af0f552f78d4694cec34f11a
mayank-ramnani <mayankr99@gmail.com>
2018-11-18 19:00:15 +0530

    first commit

---
+++ youtube_hide_home.js
@@ -0,0 +1,10 @@
+console.log("Hey mayank!");
+
+//recommended
+
+// let homeElement = document.getElementsByClassName("style-scope ytd-page-manager")[0].style.display = "none";
+document.getElementsByClassName("style-scope ytd-page-manager")[1].style.display = "none";
+// let homeElement = document.getElementsByClassName("style-scope ytd-page-manager")[2].style.display = "none";
+
+//sidebar
+document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer")[1].style.display = "none";
